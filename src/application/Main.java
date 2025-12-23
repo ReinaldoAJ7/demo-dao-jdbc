c90ff0a3ca5570c8068709ff046d38ae42627555
Reinaldo Almeida de Jesus <reinaldojesus238@gmail.com>
2025-12-23 20:41:38 -0300

    insert implementation created

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -19,7 +20,7 @@ public class Main {
 		System.out.println(seller);
 		
 		System.out.println("\n======= TEST 2: seller findById =======");
-		//Department department = new Department(2, null);
+		Department department = new Department(2, null);
 		//List<Seller> list = sellerDao.findByDepartment(department);
 		
 		//for (Seller obj : list) {
@@ -28,11 +29,50 @@ public class Main {
 		System.out.println("Verificar código SQL do findByDepartment.");
 		System.out.println("\n======= TEST 3: seller findAll =======");
 			
-			List<Seller> list = sellerDao.findAll();
+		List<Seller> list = sellerDao.findAll();
 				
 			for (Seller obj : list) {
 			System.out.println(obj);
 			}
+			
+			System.out.println("\n======= TEST 4: seller findAll =======");
+			Seller newSeller = new Seller(null,"Reinaldo","reinaldojesus238@gmail.com", new Date(), 45000.00, department);
+			sellerDao.insert(newSeller);
+			System.out.println("Inserted data! New id: " + seller.getId());
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 	}
 
 }
